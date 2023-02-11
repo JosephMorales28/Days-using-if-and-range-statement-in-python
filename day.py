a35f8edeb55b39a72ce0bb6abf2c64388450ef25
Joseph Morales <117571756+JosephMorales28@users.noreply.github.com>
2023-02-11 09:14:11 +0800

    Update

--- day.py
+++ day.py
@@ -1,28 +1,31 @@
 input1=int(input("Enter number of day:\n"))
 
-day=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+day=("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+#You can use list or tuples
 
-if input1 in range(0,8):
-   if input1 == 1:
-      print(day[0])
+if input1 in range(1,8):
+
+#   if input1 == 1:
+#      print(day[0])
      
-   elif input1 ==2:
-      print(day[1])
+#   elif input1 ==2:
+#      print(day[1])
       
-   elif input1 == 3:
-      print(day[2])
+#  elif input1 == 3:
+#      print(day[2])
       
-   elif input1 == 4:
-      print(day[3])
+#  elif input1 == 4:
+#      print(day[3])
       
-   elif input1 == 5:
-      print(day[4])
+#  elif input1 == 5:
+#      print(day[4])
       
-   elif input1 == 6:
-      print(day[5])
+#  elif input1 == 6:
+#      print(day[5])
       
-   elif input1 == 7:
-      print(day[6])
+#  elif input1 == 7:
+#      print(day[6])
       
+   print(day[input1-1])
 else:
     print("invalid")
